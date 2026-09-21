46db6fb8b2d1a6695cdb67d1b8d8c82ef829e2b6
minsung6167 <minsung1408@naver.com>
2026-09-21 14:10:52 +0900

    프로젝트 시작

---
+++ src/main/java/com/gmg1/Gmg1Application.java
@@ -0,0 +1,13 @@
+package com.gmg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gmg1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmg1/Gmg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
